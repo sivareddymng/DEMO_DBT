7f0fd5508f1ca80f255b162ac9ee108cb8418b06
Siva Alla <sivamng55@gmail.com>
2026-02-06 03:04:33 +0000

    1

---
+++ models/daily_wather.sql
@@ -0,0 +1,27 @@
+with daily_weather as (
+
+    select 
+    date(time) as daily_weather,
+    weather,
+    temp,
+    pressure,
+    humidity,
+    clouds
+    from {{ source('demo', 'weather') }}
+    limit 100
+),
+daily_weather_agg as (
+    select
+    daily_weather,
+    weather,
+    round(avg(temp),2) as avg_temp,
+    round(avg(pressure),2) as avg_pressure,
+    round(avg(clouds),2) as avg_clouds
+   /* count(weather),
+    ROW_NUMBER() OVER(PARTITION BY daily_weather order by count(weather) desc) as row_number */
+    from daily_weather
+    group by daily_weather,weather
+    qualify ROW_NUMBER() OVER(PARTITION BY daily_weather order by count(weather) desc) =1
+)
+
+select * from daily_weather_agg
